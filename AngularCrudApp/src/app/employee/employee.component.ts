d38b7517d8306bc4193a017c2c1ce9f2be836a98
Mohansai-XzBit <veeravallimohansai@gmail.com>
2022-03-14 12:35:18 +0530

    Complete Crud App

--- AngularCrudApp/src/app/employee/employee.component.ts
+++ AngularCrudApp/src/app/employee/employee.component.ts
@@ -18,8 +18,11 @@ export class EmployeeComponent implements OnInit {
 
   ngOnInit() {
     this.resetForm();
+    this.refreshEmployeeList();
   }
- 
+  onEdit(emp: Employee){
+    this.EmployeeService.selectedEmployee = emp;
+  }
   resetForm(form?:NgForm){
     if(form)
       form.reset();
@@ -32,10 +35,34 @@ export class EmployeeComponent implements OnInit {
     }
   }
   onSubmit(form: NgForm) {
-    this.EmployeeService.postEmployee(form.value).subscribe((res) =>{
-      this.resetForm(form);
-      M.toast({html: 'saved successfully', classes: 'rounded'});
-    }); 
+    if(form.value._id==""){
+      this.EmployeeService.postEmployee(form.value).subscribe((res) =>{
+        this.resetForm(form);
+        this.refreshEmployeeList();
+        M.toast({html: 'saved successfully', classes: 'rounded'});
+      });
+    }
+    else{
+      this.EmployeeService.putEmployee(form.value).subscribe((res) =>{
+        this.resetForm(form);
+        M.toast({html: 'Updated successfully', classes: 'rounded'});
+      });    
+    }
+     
   }
+  onDelete(_id:String, form:NgForm){
+    if(confirm('Confirm for deletion')==true){
+    this.EmployeeService.deleteEmployee(_id).subscribe((res) =>{
+      this.resetForm(form);
+      this.refreshEmployeeList();
+      M.toast({html: 'Deleted successfully', classes: 'rounded'});
+    }); 
+    } 
+    }
+  refreshEmployeeList(){
+  this.EmployeeService.getEmployeeList().subscribe((res) => {
+    this.EmployeeService.employees = res as Employee[];
+  });
 
+}
 }
--- AngularCrudApp/src/app/shared/employee.service.ts
+++ AngularCrudApp/src/app/shared/employee.service.ts
@@ -10,11 +10,20 @@ import { Employee } from './employee.model';
 export class EmployeeService {
   selectedEmployee :Employee;
   employees : Employee[];
-  readonly baseURL= 'http://localhost:4000/Employees'
+  readonly baseURL= 'http://localhost:4000/Employees';
 
   constructor(private http: HttpClient) { }
 
   postEmployee(emp:Employee){
     return this.http.post(this.baseURL,emp);
+  }
+  getEmployeeList(){
+    return this.http.get(this.baseURL) ;
+  }
+  putEmployee(emp: Employee){
+    return this.http.put(this.baseURL+`/${emp._id}`,emp);
+  }
+  deleteEmployee(_id:String){
+    return this.http.delete(this.baseURL+`/${_id}`);
   }
 }
